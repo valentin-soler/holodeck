8425bec5e27e387b21f2533c98acc8a0f482d930
Valentin <admin@hessfr.fr>
2025-09-03 12:27:08 +0200

    Update Script to add new fonctionnality

--- install_firewall.sh
+++ install_firewall.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 echo "Ce script va installer UFW et les différentes régles dont nous avons besoins dans Holodeck"
 
@@ -9,6 +9,8 @@ apt install ufw
 echo "Nous allons maintenant mettre en place les régles de pare-feu"
 #Par défaut, nous allons demandé à UFW de bloqué toutes les connections entrante.
 ufw default deny incomming
+sudo ufw default allow outgoing
+
 #Nous allons maintenant ouvrir certain port uniquement pour notre LAN.
 #Dans mon cas ma plage IP de mon lan est 192.168.22.0/24
 #Et je dois ouvrir les ports pour mon DHCP,DNS,FTP et serveur web vers mon LAN
@@ -27,3 +29,29 @@ ufw allow from 192.168.22.0/24 to any port 30000:30100 proto tcp comment 'FTP Da
 #Srv web
 ufw allow from 192.168.22.0/24 to any port 80 proto tcp comment 'HTTP'
 ufw allow from 192.168.22.0/24 to any port 443 proto tcp comment 'HTTPS'
+
+echo "Nous allons maintenant autorisé le passage du trafic entre du LAN vers internet"
+#Activation du forwarding IPv4
+sysctl -w net.ipv4.ip_forward=1
+if ! grep -q "^net.ipv4.ip_forward=1" /etc/sysctl.conf; then
+	echo "net.ipv4.ip_forward=1" | tee -a /etc/sysctl.conf
+fi
+
+#regles Pare-feu
+sed -i '/^*nat/,/^COMMIT/d' /etc/ufw/before.rules
+
+bash -c "cat >> /etc/ufw/before.rules" << EOF
+*nat
+:PREROUTING ACCEPT [0:0]
+:POSTROUTING ACCEPT [0:0]
+
+-A PREROUTING -i ens33 -j DNAT --to-destination 192.168.22.0/24
+-A POSTROUTING -s 192.168.22.0/24 -o ens34 -j MASQUERADE
+
+COMMIT
+EOF
+
+ufw route allow in on ens34 out on ens33
+ufw route allow in on ens33 out on ens34
+
+systemctl restart ufw
